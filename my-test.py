2f2fa3196ad2cd7b1061fbd27de072adf50b07d1
ZZheng96 <karlozone@163.com>
2026-05-05 22:08:33 +0800

    0505

--- my-test.py
+++ my-test.py
@@ -10,14 +10,13 @@ model = YOLO("../results/bridge_yolov11/weights/best.pt")
 # 2. 验证测试集（关键：指定 split="test"）
 metrics = model.val(
     data=os.path.join(code_folder, "../dataset/data.yaml"),
-    split="test"
+    split="test",
     save=True,         # 生成带检测框的图片
     save_txt=True,     # 保存检测结果txt
     imgsz=640,          # 图片尺寸（和训练一致）
     device="cpu",       # 指定CPU/GPU
     conf=0.25,          # 建议加：置信度阈值
-    iou=0.6,            # 建议加：NMS去重阈值
-    
+    iou=0.6,            # 建议加：NMS去重阈值    
     project=os.path.join(code_folder,"../results"),  # 自定义输出根目录
     name="test_result",                         # 文件夹名称
     plots=True          # 生成PR曲线、混淆矩阵
